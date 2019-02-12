854996837811551534029b9b3267e8d4dfedfc0a
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2019-02-11 23:46:07 -0700

    Make users seed only work on test env.

--- src/server/db/seeds/2_users.js
+++ src/server/db/seeds/2_users.js
@@ -1,15 +1,17 @@
 const bcrypt = require('bcryptjs');
 
 exports.seed = (knex, Promise) => {
-  const salt = bcrypt.genSaltSync();
-  const hash = bcrypt.hashSync('johnson', salt);
-  return knex('users').del()
-    .then(() => {
-      return Promise.join(
-        knex('users').insert({
-          username: 'jeremy',
-          password: hash,
-        })
-      );
-    });
+  if (process.env.NODE_ENV === 'test') {
+    const salt = bcrypt.genSaltSync();
+    const hash = bcrypt.hashSync('johnson', salt);
+    return knex('users').del()
+      .then(() => {
+        return Promise.join(
+          knex('users').insert({
+            username: 'jeremy',
+            password: hash,
+          })
+        );
+      });
+  }
 };
