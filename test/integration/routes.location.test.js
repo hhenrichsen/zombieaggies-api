cb56c5a462a6bfc7b68b00e002ade08e3304e3ae
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2019-02-19 12:12:18 -0700

    Update testing to allow for stubbing requests.

--- test/integration/routes.location.test.js
+++ test/integration/routes.location.test.js
@@ -5,10 +5,10 @@ const should = chai.should();
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
-const server = require('../src/server/index');
-const knex = require('../src/server/db/connection');
+const server = require('../../src/server/index');
+const knex = require('../../src/server/db/connection');
 
-describe('Route: Teams', () => {
+describe('Route: Locations', () => {
 
     beforeEach(() => {
         return knex.migrate.rollback()
@@ -20,12 +20,10 @@ describe('Route: Teams', () => {
         return knex.migrate.rollback();
     });
 
-    after(() => server.stop());
-
-    describe('GET /api/v1/teams', () => {
-        it('Should return all teams.', (done) => {
+    describe('GET /api/v1/locations', () => {
+        it('Should return all locations.', (done) => {
             chai.request(server)
-                .get('/api/v1/teams')
+                .get('/api/v1/locations')
                 .end((err, res) => {
                     // there should be no errors
                     should.not.exist(err);
@@ -37,12 +35,12 @@ describe('Route: Teams', () => {
                     // key-value pair of {"status": "success"}
                     res.body.status.should.eql('Success');
                     // the JSON response body should have a
-                    // key-value pair of {"data": [3 team objects]}
-                    res.body.data.length.should.eql(3);
+                    // key-value pair of {"data": [3 location objects]}
+                    res.body.data.length.should.eql(6);
                     // the first object in the data array should
                     // have the right keys
                     res.body.data[0].should.include.keys(
-                        'id', 'name', 'color', 'points'
+                        'name', 'location', 'lat', 'long', 'owner', 'active'
                     );
                     done();
                 });
@@ -50,10 +48,10 @@ describe('Route: Teams', () => {
     });
 
 
-    describe('GET /api/v1/teams/:id invalid input', () => {
-        it('Should throw an error if the team does not exist.', (done) => {
+    describe('GET /api/v1/locations/:id invalid input', () => {
+        it('Should throw an error if the location does not exist.', (done) => {
             chai.request(server)
-                .get('/api/v1/teams/-1')
+                .get('/api/v1/locations/-1')
                 .end((err, res) => {
                     // there should be a 404 status code
                     res.status.should.equal(404);
@@ -63,17 +61,17 @@ describe('Route: Teams', () => {
                     // key-value pair of {"status": "error"}
                     res.body.status.should.eql('Error');
                     // the JSON response body should have a
-                    // key-value pair of {"message": "That team does not exist."}
-                    res.body.message.should.eql('That team does not exist.');
+                    // key-value pair of {"message": "That location does not exist."}
+                    res.body.message.should.eql('That location does not exist.');
                     done();
                 });
         });
     });
 
-    describe('GET /api/v1/teams/:id', () => {
-        it('Should respond with a single team.', (done) => {
+    describe('GET /api/v1/locations/:id', () => {
+        it('Should respond with a single location.', (done) => {
             chai.request(server)
-                .get('/api/v1/teams/1')
+                .get('/api/v1/locations/1')
                 .end((err, res) => {
                     // there should be no errors
                     should.not.exist(err);
@@ -85,9 +83,9 @@ describe('Route: Teams', () => {
                     // key-value pair of {"status": "success"}
                     res.body.status.should.eql('Success');
                     // the JSON response body should have a
-                    // key-value pair of {"data": 1 team object}
+                    // key-value pair of {"data": 1 location object}
                     res.body.data.should.include.keys(
-                        'id', 'name', 'color', 'points'
+                        'name', 'location', 'lat', 'long', 'owner', 'active'
                     );
                     done();
                 });
--- test/integration/routes.teams.test.js
+++ test/integration/routes.teams.test.js
@@ -5,10 +5,10 @@ const should = chai.should();
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
-const server = require('../src/server/index');
-const knex = require('../src/server/db/connection');
+const server = require('../../src/server/index');
+const knex = require('../../src/server/db/connection');
 
-describe('Route: Locations', () => {
+describe('Route: Teams', () => {
 
     beforeEach(() => {
         return knex.migrate.rollback()
@@ -20,10 +20,12 @@ describe('Route: Locations', () => {
         return knex.migrate.rollback();
     });
 
-    describe('GET /api/v1/locations', () => {
-        it('Should return all locations.', (done) => {
+    after(() => server.stop());
+
+    describe('GET /api/v1/teams', () => {
+        it('Should return all teams.', (done) => {
             chai.request(server)
-                .get('/api/v1/locations')
+                .get('/api/v1/teams')
                 .end((err, res) => {
                     // there should be no errors
                     should.not.exist(err);
@@ -35,12 +37,12 @@ describe('Route: Locations', () => {
                     // key-value pair of {"status": "success"}
                     res.body.status.should.eql('Success');
                     // the JSON response body should have a
-                    // key-value pair of {"data": [3 location objects]}
-                    res.body.data.length.should.eql(6);
+                    // key-value pair of {"data": [3 team objects]}
+                    res.body.data.length.should.eql(3);
                     // the first object in the data array should
                     // have the right keys
                     res.body.data[0].should.include.keys(
-                        'name', 'location', 'lat', 'long', 'owner', 'active'
+                        'id', 'name', 'color', 'points'
                     );
                     done();
                 });
@@ -48,10 +50,10 @@ describe('Route: Locations', () => {
     });
 
 
-    describe('GET /api/v1/locations/:id invalid input', () => {
-        it('Should throw an error if the location does not exist.', (done) => {
+    describe('GET /api/v1/teams/:id invalid input', () => {
+        it('Should throw an error if the team does not exist.', (done) => {
             chai.request(server)
-                .get('/api/v1/locations/-1')
+                .get('/api/v1/teams/-1')
                 .end((err, res) => {
                     // there should be a 404 status code
                     res.status.should.equal(404);
@@ -61,17 +63,17 @@ describe('Route: Locations', () => {
                     // key-value pair of {"status": "error"}
                     res.body.status.should.eql('Error');
                     // the JSON response body should have a
-                    // key-value pair of {"message": "That location does not exist."}
-                    res.body.message.should.eql('That location does not exist.');
+                    // key-value pair of {"message": "That team does not exist."}
+                    res.body.message.should.eql('That team does not exist.');
                     done();
                 });
         });
     });
 
-    describe('GET /api/v1/locations/:id', () => {
-        it('Should respond with a single location.', (done) => {
+    describe('GET /api/v1/teams/:id', () => {
+        it('Should respond with a single team.', (done) => {
             chai.request(server)
-                .get('/api/v1/locations/1')
+                .get('/api/v1/teams/1')
                 .end((err, res) => {
                     // there should be no errors
                     should.not.exist(err);
@@ -83,9 +85,9 @@ describe('Route: Locations', () => {
                     // key-value pair of {"status": "success"}
                     res.body.status.should.eql('Success');
                     // the JSON response body should have a
-                    // key-value pair of {"data": 1 location object}
+                    // key-value pair of {"data": 1 team object}
                     res.body.data.should.include.keys(
-                        'name', 'location', 'lat', 'long', 'owner', 'active'
+                        'id', 'name', 'color', 'points'
                     );
                     done();
                 });
