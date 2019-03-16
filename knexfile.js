64fe589413959cdcc9cf427f1e1d6e2f660ca207
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2019-03-16 03:02:34 -0600

    Add production DB config.

--- knexfile.js
+++ knexfile.js
@@ -3,24 +3,34 @@ const path = require('path');
 const BASE_PATH = path.join(__dirname, 'src', 'server', 'db');
 
 module.exports = {
-  test: {
-    connection: 'postgres://postgres:hunter2@localhost:5432/zombieaggies_test',
-    migrations: {
-        directory: path.join(BASE_PATH, 'migrations'),
+    test: {
+        connection: 'postgres://postgres:hunter2@localhost:5432/zombieaggies_test',
+        migrations: {
+            directory: path.join(BASE_PATH, 'migrations'),
+        },
+        seeds: {
+            directory: path.join(BASE_PATH, 'seeds'),
+        },
+        client: 'pg',
     },
-    seeds: {
-        directory: path.join(BASE_PATH, 'seeds'),
+    development: {
+        connection: 'postgres://postgres:hunter2@localhost:5432/zombieaggies_dev',
+        migrations: {
+            directory: path.join(BASE_PATH, 'migrations'),
+        },
+        seeds: {
+            directory: path.join(BASE_PATH, 'seeds'),
+        },
+        client: 'pg',
     },
-      client: 'pg',
-  },
-  development: {
-    connection: 'postgres://postgres:hunter2@localhost:5432/zombieaggies_dev',
-    migrations: {
-        directory: path.join(BASE_PATH, 'migrations'),
+    production: {
+        connection: process.env.DATABASE_URL,
+        migrations: {
+            directory: path.join(BASE_PATH, 'migrations'),
+        },
+        seeds: {
+            directory: path.join(BASE_PATH, 'seeds'),
+        },
+        client: 'pg',
     },
-    seeds: {
-        directory: path.join(BASE_PATH, 'seeds'),
-    },
-      client: 'pg',
-  },
 };
