7978f1407565c954bb80956f49394227bcd65362
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2019-03-24 16:54:07 -0600

    Move map to index router.

--- src/server/routes/public/index.js
+++ src/server/routes/public/index.js
@@ -12,4 +12,18 @@ router.get('/rules', async ctx =>
     await ctx.render("rules");
 });
 
+router.get('/map/', async ctx =>
+{
+    let date = new Date();
+    if ((date.getMonth() === 4 && date.getDay() >= 2) ||
+        (ctx.isAuthenticated() && ctx.req.user.permissions.viewHiddenTabs))
+    {
+        await ctx.render("map");
+    }
+    else
+    {
+        ctx.status = 404;
+    }
+});
+
 module.exports = router;
