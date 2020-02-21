e0598bb66e19097163148a630bb1d2831ee73afc
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2020-02-21 11:39:32 -0700

    Speed pass.

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,11 @@ const postcss      = require('gulp-postcss');
 const sourcemaps   = require('gulp-sourcemaps');
 const autoprefixer = require('autoprefixer');
 const cssnano      = require('cssnano');
+const gzip         = require('gulp-gzip')
 
+const gz_leave_src = function () {
+
+}
 
 const css = function ()
 {
@@ -27,6 +31,17 @@ const css = function ()
         .pipe(sourcemaps.write('dist/maps'))
         .pipe(dest('dist'));
 };
+
+const cssgz = function() {
+    return src('static/scss/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed',
+        }).on('error', sass.logError))
+        .pipe(postcss([ autoprefixer(), cssnano() ]))
+        .pipe(gzip())
+        .pipe(dest('dist'));
+}
 
 const js = function ()
 {
@@ -41,6 +56,20 @@ const js = function ()
         .pipe(sourcemaps.write('dist/maps'))
         .pipe(dest('dist'));
 };
+
+const jsgz = function () {
+    return src('static/js/*.js')
+        .pipe(sourcemaps.init())
+        .pipe(minify({
+            ext: {
+                src: '-src.js',
+                min: '.js',
+            },
+        }))
+        .pipe(sourcemaps.write('dist/maps'))
+        .pipe(gzip())
+        .pipe(dest('dist'));
+}
 
 const clean = function ()
 {
@@ -64,10 +93,16 @@ const copy = function ()
         .pipe(dest('./dist/'));
 };
 
+const copygz = function () {
+    return src('./static/copy/*')
+        .pipe(gzip())
+        .pipe(dest('./dist'));
+}
+
 
 const build = async function (cb)
 {
-    await series(clean, parallel(css, js, copy))();
+    await series(clean, parallel(css, cssgz, js, jsgz, copy, copygz))();
     return cb();
 };
 
