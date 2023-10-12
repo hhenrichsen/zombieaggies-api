2ea72daebe111bc45e5154e529e27061c76e4895
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2023-10-11 20:21:44 -0600

    fix OZ related code so it only works for one team

--- src/server/routes/api/tags.js
+++ src/server/routes/api/tags.js
@@ -1,79 +1,83 @@
-const logger = require('../../logger').default
+const logger = require("../../logger").default;
 
-const events = require('../../../db/queries/events')
-const tags = require('../../../db/queries/tags')
+const events = require("../../../db/queries/events");
+const tags = require("../../../db/queries/tags");
+const users = require("../../../db/queries/users");
 
-const RateLimit = require('koa2-ratelimit').RateLimit
+const RateLimit = require("koa2-ratelimit").RateLimit;
 
-const Router = require('koa-router')
+const Router = require("koa-router");
 
-const bot = require('../../../discord/bot')
+const bot = require("../../../discord/bot");
 
 const tagRateLimit = RateLimit.middleware({
   interval: 5 * 60 * 1000, // 5 minutes
   max: 5,
-  prefixKey: 'tags' // to allow the bdd to Differentiate the endpoint
-})
+  prefixKey: "tags", // to allow the bdd to Differentiate the endpoint
+});
 
-const router = new Router()
-const BASE_URL = `/tags`
+const router = new Router();
+const BASE_URL = `/tags`;
 
-router.get(`${BASE_URL}`, async ctx => {
-  let _events = await events.getEventsFromVerb('tagged')
+router.get(`${BASE_URL}`, async (ctx) => {
+  let _events = await events.getEventsFromVerb("tagged");
   ctx.body = await Promise.all(
-    _events.map(async event => {
-      if (await tags.isOZ(event.subject)) {
-        event.subject = 'OZ'
+    _events.map(async (event) => {
+      if (
+        (await tags.isOZ(event.subject)) &&
+        (await users.getUser(event.subject)).team === 2
+      ) {
+        event.subject = "OZ";
       }
-      return event
+      return event;
     })
-  )
-  ctx.status = 200
-  return Promise.resolve()
-})
+  );
+  ctx.status = 200;
+  return Promise.resolve();
+});
 
-router.post(`${BASE_URL}/add`, tagRateLimit, async ctx => {
+router.post(`${BASE_URL}/add`, tagRateLimit, async (ctx) => {
   if (ctx.isAuthenticated()) {
     if (ctx.request.body.code !== undefined) {
-      let id = await tags.getIdFromCode(ctx.request.body.code.toUpperCase())
+      let id = await tags.getIdFromCode(ctx.request.body.code.toUpperCase());
       if (id.length === 1) {
         return await tags
           .tagUser(ctx.req.user.id, id[0].user)
-          .then(async user => {
-            await bot.getInstance().updateUser(user)
-            return user
+          .then(async (user) => {
+            await bot.getInstance().updateUser(user);
+            return user;
           })
           .then(async () => {
-            ctx.status = 200
-            ctx.body = 'Success!'
-            ctx.redirect('/tags')
-            return Promise.resolve()
+            ctx.status = 200;
+            ctx.body = "Success!";
+            ctx.redirect("/tags");
+            return Promise.resolve();
           })
-          .catch(err => {
-            ctx.status = 400
+          .catch((err) => {
+            ctx.status = 400;
             ctx.body = {
-              message: err.message
-            }
-            return Promise.resolve()
-          })
+              message: err.message,
+            };
+            return Promise.resolve();
+          });
       } else {
-        ctx.status = 400
+        ctx.status = 400;
         ctx.body = {
-          message: 'Invalid code.'
-        }
-        return Promise.resolve()
+          message: "Invalid code.",
+        };
+        return Promise.resolve();
       }
     } else {
-      ctx.status = 400
+      ctx.status = 400;
       ctx.body = {
-        message: 'Property `code` is required.'
-      }
-      return Promise.resolve()
+        message: "Property `code` is required.",
+      };
+      return Promise.resolve();
     }
   } else {
-    ctx.status = 401
-    return Promise.resolve()
+    ctx.status = 401;
+    return Promise.resolve();
   }
-})
+});
 
-module.exports = router
+module.exports = router;
--- src/server/routes/public/index.js
+++ src/server/routes/public/index.js
@@ -1,111 +1,114 @@
-const events = require('../../../db/queries/events')
-const users = require('../../../db/queries/users')
-const tags = require('../../../db/queries/tags')
-const teams = require('../../../db/queries/teams')
-const lore = require('../../../db/queries/lore')
+const events = require("../../../db/queries/events");
+const users = require("../../../db/queries/users");
+const tags = require("../../../db/queries/tags");
+const teams = require("../../../db/queries/teams");
+const lore = require("../../../db/queries/lore");
 
-const Router = require('koa-router')
+const Router = require("koa-router");
 
-const router = new Router()
+const router = new Router();
 
-router.get('/', async ctx => {
-  await ctx.render('home', {
-    googleVerification: process.env['GOOGLE_VERIFICATION']
-  })
-})
+router.get("/", async (ctx) => {
+  await ctx.render("home", {
+    googleVerification: process.env["GOOGLE_VERIFICATION"],
+  });
+});
 
-router.get('/rules', async ctx => {
-  await ctx.render('rules')
-})
+router.get("/rules", async (ctx) => {
+  await ctx.render("rules");
+});
 
-router.get('/lore', async ctx => {
+router.get("/lore", async (ctx) => {
   if (ctx.isAuthenticated()) {
-    const list = await lore.getAllUnlocked()
-    await ctx.render('lore', {
+    const list = await lore.getAllUnlocked();
+    await ctx.render("lore", {
       lore: list,
-      csrf: ctx.csrf
-    })
+      csrf: ctx.csrf,
+    });
   } else {
-    return ctx.redirect('/auth/login')
+    return ctx.redirect("/auth/login");
   }
-})
+});
 
-router.get('/lore/:id', async ctx => {
+router.get("/lore/:id", async (ctx) => {
   if (ctx.isAuthenticated()) {
-    const list = await lore.get(ctx.params.id)
-    await ctx.render('loreItem', {
+    const list = await lore.get(ctx.params.id);
+    await ctx.render("loreItem", {
       lore: list,
-      csrf: ctx.csrf
-    })
+      csrf: ctx.csrf,
+    });
   } else {
-    return ctx.redirect('/auth/login')
+    return ctx.redirect("/auth/login");
   }
-})
+});
 
-router.get('/map', async ctx => {
+router.get("/map", async (ctx) => {
   if (ctx.isAuthenticated() && ctx.req.user.permissions.accessPointManagement) {
-    await ctx.redirect('/manage')
+    await ctx.redirect("/manage");
   } else {
-    await ctx.render('map')
+    await ctx.render("map");
   }
-})
+});
 
-router.get('/tags', async ctx => {
+router.get("/tags", async (ctx) => {
   if (ctx.isAuthenticated()) {
-    let ev = await events.getEventsFromVerb('tagged')
+    let ev = await events.getEventsFromVerb("tagged");
     let evs = await Promise.all(
-      ev.map(async event => {
+      ev.map(async (event) => {
         let obj = {
           actor: {},
           target: {},
           team: 0,
-          time: event.createdAt
-        }
+          time: event.createdAt,
+        };
 
-        obj.team = event.info.team
+        obj.team = event.info.team;
 
-        const actor = await users.getUser(event.subject)
+        const actor = await users.getUser(event.subject);
         if (actor) {
-          if (await tags.isOZ(event.subject)) {
-            obj.actor.name = 'OZ'
+          if (
+            ((await tags.isOZ(event.subject)) &&
+              (await users.getUser(event.subject)).team) === 2
+          ) {
+            obj.actor.name = "OZ";
           } else {
             obj.actor.name =
               actor.firstname +
-              (actor.nickname ? ' "' + actor.nickname + '"' : '') +
-              ' ' +
-              actor.lastname
-            obj.actor.id = actor.id
+              (actor.nickname ? ' "' + actor.nickname + '"' : "") +
+              " " +
+              actor.lastname;
+            obj.actor.id = actor.id;
           }
         } else {
-          obj.actor.name = '[deleted]'
+          obj.actor.name = "[deleted]";
         }
 
-        const target = await users.getUser(parseInt(event.target))
+        const target = await users.getUser(parseInt(event.target));
         if (target) {
           obj.target.name =
             target.firstname +
-            (target.nickname ? ' "' + target.nickname + '"' : '') +
-            ' ' +
-            target.lastname
-          obj.target.id = parseInt(event.target)
+            (target.nickname ? ' "' + target.nickname + '"' : "") +
+            " " +
+            target.lastname;
+          obj.target.id = parseInt(event.target);
         } else {
-          obj.target.name = '[deleted]'
+          obj.target.name = "[deleted]";
         }
-        return obj
+        return obj;
       })
-    )
+    );
 
-    let teamList = await teams.getAllTeams()
+    let teamList = await teams.getAllTeams();
 
-    await ctx.render('tags', {
+    await ctx.render("tags", {
       user: ctx.req.user,
       csrf: ctx.csrf,
       tags: evs,
-      teams: teamList
-    })
+      teams: teamList,
+    });
   } else {
-    return ctx.redirect('/auth/login')
+    return ctx.redirect("/auth/login");
   }
-})
+});
 
-module.exports = router
+module.exports = router;
